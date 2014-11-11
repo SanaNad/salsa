cec8a10662ac0459ab00669c09fa5f3e3a87e28e
root <root@debian.Home>
2014-11-11 20:27:04 +0300

    rostik

--- main.c
+++ main.c
@@ -7,17 +7,46 @@
 #include <string.h>
 #include <stdint.h>
 
+#include <sys/time.h>
+
 #include "salsa.h"
 
-#define	BUFLEN	1000
+#define	BUFLEN	5000000
 #define KEYLEN	32
+
+// Struct for time value
+struct timeval t1, t2, dtv;
+
+// Function time measurement
+void
+time_start()
+{
+	gettimeofday(&t1, NULL);
+}
+
+// Function time measurement
+long
+time_stop()
+{
+	gettimeofday(&t2, NULL);
+
+	dtv.tv_sec = t2.tv_sec - t1.tv_sec;
+	dtv.tv_usec = t2.tv_usec - t1.tv_usec;
+
+	if(dtv.tv_usec < 0) {
+		dtv.tv_sec--;
+		dtv.tv_usec += 1000000;
+	}
+
+	return dtv.tv_sec * 1000 + dtv.tv_usec/1000;
+}
 
 int main()
 {
 	struct salsa_context *ctx;
 	uint8_t *buf, nonce[8], key[KEYLEN];
 	char s[BUFLEN];
-	int buflen, keylen, i;
+	long int buflen, keylen;
 	
 	keylen = KEYLEN;
 	buflen = BUFLEN;
@@ -25,7 +54,9 @@ int main()
 	memset(s, 'q', sizeof(s));
 	memset(key, 'k', sizeof(key));
 	memset(nonce, 1, sizeof(nonce));
-
+	
+	time_start();
+	
 	if((ctx = salsa_context_new()) == NULL) {
 		printf("Memory allocation error!\n");
 		exit(1);
@@ -37,21 +68,18 @@ int main()
 		printf("Salsa context filling error!\n");
 		exit(1);
 	}
-
-	salsa_encrypt(ctx, (uint8_t *)s, buflen);
 	
-	// print encrypted data
-	for(i = 0; i < buflen; i++)
-		printf(" %c -", buf[i]);
-	printf("\n\n");
+	salsa_encrypt(ctx, (uint8_t *)s, buflen);
+
+	if(salsa_set_key_and_nonce(ctx, (uint8_t *)key, keylen, nonce)) {
+		printf("Salsa context filling error!\n");
+		exit(1);
+	}
 
 	salsa_decrypt(ctx, buf, buflen);
 
-	// print decrypted data
-	for(i = 0; i < buflen; i++)
-		printf(" %c ", buf[i]);
-	printf("\n\n");
-
+	printf("Run time = %ld\n\n", time_stop());
+	
 	salsa_context_free(&ctx);
 
 	return 0;
