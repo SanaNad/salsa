b8f40d9489796066e3382fb80ccabb95ad00fdc9
root <root@debian.Home>
2014-11-25 22:37:03 +0300

    rostik

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
-/* This program tests the library Salsa20 - salsa.h
- * Minimum data. Only for test.
-*/
+// This program tests the library salsa.h
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
-
+#include <string.h>
 #include <sys/time.h>
 
 #include "salsa.h"
@@ -15,43 +12,45 @@
 #define KEYLEN	32
 
 // Struct for time value
-struct timeval t1, t2, dtv;
+struct timeval t1, t2;
+
+uint8_t buf[BUFLEN];
+uint8_t out1[BUFLEN];
+uint8_t out2[BUFLEN];
+uint8_t key[KEYLEN];
+uint8_t nonce[8];
 
 // Function time measurement
-void
-time_start()
+static void
+time_start(void)
 {
 	gettimeofday(&t1, NULL);
 }
 
 // Function time measurement
-long
-time_stop()
+static uint32_t
+time_stop(void)
 {
 	gettimeofday(&t2, NULL);
 
-	dtv.tv_sec = t2.tv_sec - t1.tv_sec;
-	dtv.tv_usec = t2.tv_usec - t1.tv_usec;
+	t2.tv_sec -= t1.tv_sec;
+	t2.tv_usec -= t1.tv_usec;
 
-	if(dtv.tv_usec < 0) {
-		dtv.tv_sec--;
-		dtv.tv_usec += 1000000;
+	if(t2.tv_usec < 0) {
+		t2.tv_sec--;
+		t2.tv_usec += 1000000;
 	}
 
-	return dtv.tv_sec * 1000 + dtv.tv_usec/1000;
+	return (t2.tv_sec * 1000 + t2.tv_usec/1000);
 }
 
-int main()
+int 
+main()
 {
 	struct salsa_context *ctx;
-	uint8_t *buf, nonce[8], key[KEYLEN];
-	char s[BUFLEN];
-	long int buflen, keylen;
-	
-	keylen = KEYLEN;
-	buflen = BUFLEN;
-	
-	memset(s, 'q', sizeof(s));
+	uint32_t i;
+
+	memset(buf, 'q', sizeof(buf));
 	memset(key, 'k', sizeof(key));
 	memset(nonce, 1, sizeof(nonce));
 	
@@ -62,23 +61,21 @@ int main()
 		exit(1);
 	}
 	
-	buf = (uint8_t *)&s[0];
-
-	if (salsa_set_key_and_nonce(ctx, (uint8_t *)key, keylen, nonce)) {
+	if (salsa_set_key_and_nonce(ctx, (uint8_t *)key, KEYLEN, nonce)) {
 		printf("Salsa context filling error!\n");
 		exit(1);
 	}
 	
-	salsa_encrypt(ctx, (uint8_t *)s, buflen);
-
-	if(salsa_set_key_and_nonce(ctx, (uint8_t *)key, keylen, nonce)) {
+	salsa_encrypt(ctx, buf, BUFLEN, out1);
+	
+	if(salsa_set_key_and_nonce(ctx, (uint8_t *)key, KEYLEN, nonce)) {
 		printf("Salsa context filling error!\n");
 		exit(1);
 	}
 
-	salsa_decrypt(ctx, buf, buflen);
-
-	printf("Run time = %ld\n\n", time_stop());
+	salsa_decrypt(ctx, out1, BUFLEN, out2);
+	
+	printf("Run time = %d\n\n", time_stop());
 	
 	salsa_context_free(&ctx);
 
