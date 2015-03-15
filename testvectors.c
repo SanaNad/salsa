99138eecf61021dd89df75eacd04d8c9d74b05b4
root <root@debian.Home>
2015-03-15 13:48:48 +0300

    rostik

--- testvectors.c
+++ testvectors.c
@@ -38,14 +38,21 @@ main(void)
 		exit(1);
 	}
 
-	if(salsa_set_key_and_iv(ctx, key1, 32, iv1)) {
+	if(salsa_set_key_and_iv(ctx, key1, 32, iv1, 8)) {
 		printf("Salsa context filling error!\n");
 		exit(1);
 	}
 	
 	salsa_test_vectors(ctx);
 
-	if(salsa_set_key_and_iv(ctx, key2, 32, iv2)) {
+	salsa_context_free(&ctx);
+
+	if((ctx = salsa_context_new()) == NULL) {
+		printf("Memory allocation error!\n");
+		exit(1);
+	}
+
+	if(salsa_set_key_and_iv(ctx, key2, 32, iv2, 8)) {
 		printf("Salsa context filling error!\n");
 		exit(1);
 	}
